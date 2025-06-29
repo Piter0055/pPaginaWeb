d3e55524474194392eebe573b25b171dbd609dbc
Piter0055 <pedro.eduardo.silva13@escola.pr.gov.br>
2025-06-29 14:42:49 -0300

    perguntas finalizadas e cor modificada

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,62 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Esporte',
+    'Qual a competição mais importante do mundo?',
+    'Copa do mundo FIFA'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Futebol',
+    'Em que ano o futebol foi itroduzido no Brasil?',
+    '1894'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Futebol',
+    'Quem trouxe o futebol para o Brasil?',
+    'Charles Miller'
 )
-
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
-) 
+    'Futebol',
+    'Quantas vezes o Brasil á ganhou a copa do mundo?',
+    '5 vezes'
+)
+criaCartao(
+    'Futebol',
+    'Quai as duas principais premiações do futebol?',
+    'Bola de ouro e Fifa The Best'
+)
+criaCartao(
+    'Futebol',
+    'Quem tem a maior quantidade de Bola de Ouro?',
+    'Messi com 8'
+)
+criaCartao(
+    'Futebol',
+    'Quem tem a maior quantidade de Fifa The Best?',
+    'Messi com 3'
+)
+criaCartao(
+    'Futebol',
+    'Qual time é considerado que tem a maior torcida d mundo? ',
+    'Flamengo'
+)
+criaCartao(
+    'Futebol',
+    'Quem é o maior artilheiro do mundo?',
+    'Cristiano Ronaldo'
+)
+criaCartao(
+    'Futebol',
+    'Quem é o maior artilheiro do Brasil?',
+    'Neymar (79 gols em 125 jogos)'
+)
+criaCartao(
+    'Futebol',
+    'Qual é a importância do futebol?',
+    'futebol é mais do que apenas um esporte; é uma força social, cultural e econômica que impacta a vida das pessoas de diversas maneiras, promovendo a saúde, a socialização e o desenvolvimento.'
+)
+criaCartao(
+    'Futebol',
+    'Porque o mascote do Flamengo é um urubu?',
+    'O urubu é um dos símbolos do Flamengo e sua história está intrinsecamente ligada à torcida. O urubu surgiu como um insulto racista, mas a torcida rubro-negra ressignificou o animal, transformando-o num símbolo de orgulho e identidade.'
+)
